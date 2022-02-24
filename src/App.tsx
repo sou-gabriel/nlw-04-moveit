05c978cd444c9be0dd3ea3c895342fcf4d7ec289
Gabriel Ramos Nogueira <69519457+sou-gabriel@users.noreply.github.com>
2022-02-24 10:20:20 -0300

    NLW#4 - Workshop 01 React

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ExperienceBar } from './components/ExperienceBar'
+
+import './styles/global.css'
+
+export const App = () => {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  )
+}
